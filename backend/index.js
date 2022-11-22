7397557cf1b065526c8f5e8afb16cee1cca423b3
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2022-11-22 10:52:59 +0545

    add database code

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,57 @@
 import express from "express";
+import cors from "cors";
+import mysql from "mysql";
 
 import mainRoutes from "./routes/main.js";
 import userRoutes from "./routes/userRoutes.js"
 
 //Intialize the app with express
 const app = express();
-
-//middleware
+app.use(cors());
 app.use(express.json());
 
-//routes
+// Setup the database connection
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    // database: "vehicle_gps"
+    database: "myportfolio"
+});
+
+//middleware to connect to the database
+app.use((req, res, next) => {
+    req.db = db;
+    next();
+});
+
+// Test whether the query can be executed or not.
+app.get("/testcontact", (req, res) => {
+    //query to get the selected data from the database
+    const sql = "SELECT * FROM contact";
+
+    //execute the query
+    db.query(sql, (err, result) => {
+        if (err) { throw err; }
+        res.send(result);
+    });
+});
+
+// Test the get request to the server
+app.get("/showusers", (req, res) => {
+    const sql = "SELECT * FROM users";
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+});
+
+
+// Setup the routes
 app.use("/api", mainRoutes);
-app.use("/api/user", userRoutes);
+app.use("api/user", userRoutes);
+
+
 
 
 // Define the port where the server should listen
