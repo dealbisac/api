8554b5870ed797b54d94bc00716be4e59c597705
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2022-11-28 10:42:08 +0545

    feature: add login with db

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,24 +1,45 @@
-// user model is imported.
-
-import { json } from "express";
+import db from "../models/main.js";
+// import { json } from "express";
 
 export const login = (req, res) => {
+    // OLD WAY OF DOING IT
     // When the model is implemented, the data provided by the user
     // is compared with the data in database and acts accordingly.
     // const matched = true;
     // get json data from the request body
+    // const { username, password } = req.body;
+    // if(username==="dipendra" && password==="dipendra123"){
+    //     res.send("Successfully Logged in")
+    // } else {
+    //     res.send ("Username or Password does not match.")
+    // }
+
+    // NEW WAY WHEN DATABASE IS IMPLEMENTED
+    // get json data from the request body
     const { username, password } = req.body;
-    if(username==="dipendra" && password==="dipendra123"){
-        res.send("Successfully Logged in")
-    } else {
-        res.send ("Username or Password does not match.")
-    }
+
+    // prepare the query
+    const sql = "SELECT * FROM `user` WHERE username = ? AND password = ?";
+    db.query(sql, [username, password], (err, result) => {
+        if (err) throw err;
+        if (result.length > 0) {
+            res.send("Successfully Logged in");
+        } else {
+            res.send("Username or Password does not match.");
+        }
+    });
 }
 
 export const register = (req, res) => {
-    // the data provided by the user is stored in the database.
-
     // get json data from the request body
     const { name, address, username, password } = req.body;
+
+    // prepare the query
+    const sql = "INSERT INTO `user`(`name`, `address`, `username`, `password`) VALUES (?,?,?,?)";
+    db.query
+    (sql, [name, address, username, password], (err, result) => {
+        if (err) throw err;
+        res.send("Data inserted sucessfully");
+    });
     res.send("User successfully created" + "\n" + "Name: " + name + "\n" + "Address: " + address + "\n" + "Username: " + username + "\n" + "Password: " + password);
 }
--- backend/index.js
+++ backend/index.js
@@ -10,53 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Setup the database connection
-// const db = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     // database: "vehicle_gps"
-//     database: "myportfolio"
-// });
-
-//middleware to connect to the database
-// app.use((req, res, next) => {
-//     req.db = db;
-//     next();
-// });
-
-// Test whether the query can be executed or not.
-// app.get("/testcontact", (req, res) => {
-//     //query to get the selected data from the database
-//     const sql = "SELECT * FROM contact";
-
-//     //execute the query
-//     db.query(sql, (err, result) => {
-//         if (err) { throw err; }
-//         res.send(result);
-//     });
-// });
-
-// Test the get request to the server
-// app.get("/showusers", (req, res) => {
-//     const sql = "SELECT * FROM users";
-//     db.query(sql, (err, result) => {
-//         if (err) throw err;
-//         res.send(result);
-//     });
-// });
-
-//Test the get request to the server to get education details
-// app.get("/showeducation", (req, res) => {
-//     const sql = "SELECT * FROM education";
-//     db.query(sql, (err, result) => {
-//         if (err) throw err;
-//         res.send(result);
-//     });
-// });
-
-
-
 // Setup the routes
 app.use("/api", mainRoutes);
 app.use("api/user", userRoutes);
